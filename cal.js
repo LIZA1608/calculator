e0b05f0945af1f9e5edb20ffcf3a40e4232ee342
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-09-15 13:49:55 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,21 +1,3 @@
-// class Calculator{
-//     constructor(reviousOperandandTextElement,currentOperandandTextElement ){
-//         this.previousOperandandTextElement=previousOperandandTextElement
-//         this.currentOperandandTextElement=currentOperandandTextElement
-
-//     }
-// }
-
-
-
-// const numberButton = document.querySelectorAll('[data-number]')
-// const operationsButton = document.querySelectorAll('[data-operations]')
-// const equalsButton = document.querySelectorAll('[data-equals]')
-// const deleteButton = document.querySelectorAll('[data-delete]')
-// const allClearButton = document.querySelectorAll('[data-all-clear]')
-// const previousOperandandTextElement = document.querySelectorAll('[data-previous-operand]')
-// const currentOperandandTextElement= document.querySelectorAll('[data-current-operand]')
-
 const inputE=document.querySelector('input')
 const buttonE=document.querySelectorAll('button')
 //to select all the button 
@@ -37,14 +19,7 @@ for(let button of buttonE){
             console.log('Invalid')
         }
     }
-    // else if(text==='Back'){
-    //   const temp=inputE.value
-    //   var next=""
-    //   for(var i=0;i<temp.length-1;i++){
-    //     next+=temp[i];
-    //   }
-    //   inputE.value=next
-    // }
+  
     else if(text==='Back'){
         inputE.value=inputE.value.slice(0,-1)
         //slice cut the string and return the new string 
